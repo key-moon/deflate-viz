e16bee3d393efeec8c3a8d28b50ff51fc2ebdb3a
key-moon <kymn0116@gmail.com>
2025-08-14 08:26:17 +0900

    a

--- dist/gzip_zopfli_worker_script.mjs
+++
@@ -1,5 +0,0 @@
-import {gunzip,gzip,zopfli} from "./gzip_zopfli.mjs";
-onmessage=async({data})=>postMessage(
-  data.level===undefined?(data.zopfli?zopfli(data.bytes):gunzip(data)):gzip(data.bytes,data.level)
-);
-postMessage('ready');
